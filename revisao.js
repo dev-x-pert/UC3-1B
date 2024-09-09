778aad60e75b7a6656d0a83bdade8cb3b4479fc9
dev-x-pert <166012851+dev-x-pert@users.noreply.github.com>
2024-09-09 08:10:53 -0300

    Update revisao.js

--- revisao.js
+++ revisao.js
@@ -1,1 +1,8 @@
+const livros = "livro1, livro2, livro3".split(", ")
+const maislivros = "livro4, livro5, livro6".split(", ")
 
+console.log(livros)
+
+livros.push(...maislivros)
+
+console.log(livros)
